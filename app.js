45bb9ba6038e69e6999ae55009ea7ae9168add1c
Manuja Niroshan <manujaniroshan5@gmail.com>
2025-07-29 22:13:28 +0530

    files

---
+++ app.js
@@ -0,0 +1,73 @@
+//step 1: get DOM
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
+
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
+
+// Add to app.js
+function openReviewsPopup(url) {
+    const popup = document.getElementById('reviewsPopup');
+    const iframe = document.getElementById('reviewsIframe');
+    iframe.src = url;
+    popup.style.display = 'flex';
+    
+    // Disable buttons in iframe
+    iframe.onload = () => {
+        const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+        const buttons = iframeDoc.getElementsByTagName('button');
+        for (let button of buttons) {
+            button.disabled = true;
+            button.style.pointerEvents = 'none';
+        }
+    };
+}
+
+function closeReviewsPopup() {
+    const popup = document.getElementById('reviewsPopup');
+    popup.style.display = 'none';
+    document.getElementById('reviewsIframe').src = '';
+}
